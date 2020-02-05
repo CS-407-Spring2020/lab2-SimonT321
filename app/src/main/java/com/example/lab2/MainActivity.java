7912644b3a794f936e29575c5d3363ec9071e4cc
SimonT321 <smtheuer@gmail.com>
2020-02-05 11:28:16 -0800

    Got to step 16. Not sure if Toast is working as intended.

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        Log.i("Info", "Button pressed");
+        EditText myTextField = (EditText) findViewById(R.id.editText);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
